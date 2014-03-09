c82dad1aa697fa7ea5a306f9fa9f860305eebce5
Amie Kweon <amiekweon@gmail.com>
2014-03-09 15:28:31 -0700

    Initial Commit

---
+++ JabberClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JabberClient
+//
+//  Created by Amie Kweon on 3/9/14.
+//  Copyright (c) 2014 Amie Kweon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
